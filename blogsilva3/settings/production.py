772a9fa9f35b8afee829a9c0db1edc193d010e55
DiegoSilva2346 <diegenriquesilva@gmail.com>
2020-11-02 17:27:49 -0300

    correccion de host permitidos en produccion

--- blogsilva3/settings/production.py
+++ blogsilva3/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://git.heroku.com/blogsilva3.git"]
+ALLOWED_HOSTS = ["blogsilva3.herokuapp.com"]
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
